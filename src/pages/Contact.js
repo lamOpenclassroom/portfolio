af970017b9c2215a11e081698bb5cb4c50be2c49
lamtaoui mohamed <el-hafid-@hotmail.fr>
2024-01-11 15:53:06 +0100

    parameter correction Netlify 2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,22 @@
+import { useState } from "react"
 import "../assets/style/contact.scss"
 
 function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("")
     return (
         <div id="block-contact">
             <h1>Contact</h1>
 
             <form name="contact" method="POST" id="block-form">
-            <input type="hidden" name="form-name" value="contact"/>
+                <input type="hidden" name="form-name" value="contact"/>
                 <label for="name"> Nom :</label>
-                    <input className="effet" type="text" name="name" value="name"/>
+                <input className="effet" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
                 <label for="email"> Email :</label>
-                    <input className="effet" type="email" name="email" value="email"/>
+                    <input className="effet" type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <label for="message"> Message :</label>
-                    <textarea className="effet" name="message" value="message"></textarea>
+                    <textarea className="effet" name="message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
             </form>
 
             <button type="submit">Envoyer</button>
