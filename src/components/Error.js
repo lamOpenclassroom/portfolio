007e0ea7273ef2c8b3686535c513d9ea49177796
lamtaoui mohamed <el-hafid-@hotmail.fr>
2024-01-12 16:16:28 +0100

    page error fixed 5 + add error.html

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,15 +1,13 @@
 import { Link } from "react-router-dom";
 import "../assets/style/Message.scss";
-import { useLocation } from "react-router-dom";
 
 function Error() {
-    const location = useLocation();
 
     return (
         <div className="bloc-message">
             <Link id="message-retour" to="/">Retour à l'acceuil</Link>
             <div >
-                <h1 className="message-title">"{location.pathname}"Page not found</h1>
+                <h1 className="message-title">Page not found</h1>
                 <p className="message-title">404</p>
             </div>
         </div>
