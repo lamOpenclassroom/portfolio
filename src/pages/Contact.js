b384084cc5d14c708095232e76e23410203de2d5
lamtaoui mohamed <el-hafid-@hotmail.fr>
2024-01-12 10:11:59 +0100

    change form path

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,19 +9,21 @@ function Contact() {
         <div id="block-contact">
             <h1>Contact</h1>
 
-            <form name="contact" method="POST" id="block-form">
-                <input type="hidden" name="form-name" value="contact"/>
-                <label for="name"> Nom :</label>
-                <input className="effet" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
-                <label for="email"> Email :</label>
+            <form action="/sent" name="contact" method="POST" id="form">
+                <input type="hidden" name="form-name" value="contact" />
+                <div id="block-form">
+                    <label for="name"> Nom :</label>
+                    <input className="effet" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <label for="email"> Email :</label>
                     <input className="effet" type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <label for="message"> Message :</label>
-                <textarea
-                    className="effet"
-                    name="message"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}>
-                </textarea>
+                    <label for="message"> Message :</label>
+                    <textarea
+                        className="effet"
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}>
+                    </textarea>
+                </div>
                 <button type="submit">Envoyer</button>
             </form>
 
