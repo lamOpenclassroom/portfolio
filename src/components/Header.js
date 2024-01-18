f2fe3a45d006f9569344f547629b14db79d3a46f
lamtaoui mohamed <el-hafid-@hotmail.fr>
2024-01-18 11:20:55 +0100

    Hedaer responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-
+import {useState} from "react";
 import { Link } from "react-router-dom";
 import "../assets/style/header.scss";
 
 function Header() {
+    const [menuMobil, setmenuMobil] = useState(true);
+    
+    const addMenu = () => {
+        setmenuMobil(!menuMobil);
+    }
+
     return (
         <header className="header">
             <div className='title'>
                 <h1>LAMTAOUI MOHAMMED</h1>
                 <h2>Developpeur Web</h2>
             </div>
-            
             <div className="navigation">
                 <nav>
                     <Link className="link" to="/">Acceuil</Link>
@@ -18,6 +23,16 @@ function Header() {
                     <Link className="link" to="/contact">Contact</Link>
                 </nav>
             </div>
+            <div className={menuMobil ? "hidden" : "visible navMobil"}>
+                <nav className="navMobil-link">
+                    <Link onClick={addMenu} className="link" to="/">Acceuil</Link>
+                    <Link onClick={addMenu} className="link" to="/project">Mes projets</Link>
+                    <Link onClick={addMenu} className="link" to="/techno">Les technologies</Link>
+                    <Link onClick={addMenu} className="link" to="/contact">Contact</Link>
+                </nav>
+            </div>
+            
+            <i onClick={addMenu} className="fa-solid fa-bars"></i>
         </header>
     )
 }
